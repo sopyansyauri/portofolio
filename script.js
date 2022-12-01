a2c5f3621de495aa0b608ab371f72ef0a69ace4a
sopyan <rifai.sopyan04@gmail.com>
2022-12-01 10:55:07 +0700

    Program Portofolio

---
+++ script.js
@@ -0,0 +1,26 @@
+const textElement = ["I'm FrontEnd Developer"];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = "";
+let words = "";
+
+function ngetik() {
+    if(count == textElement.length) {
+        count = 0;
+    }
+
+    currentTxt = textElement[count];
+
+    words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector("#jurusan").textContent = words;
+
+    if(words.length == currentTxt.length) {
+        count++;
+        txtIndex = 0;
+    }
+
+    setTimeout(ngetik, 400);
+
+}
+
+ngetik();
